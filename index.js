b5ca97bb51147e0e665515df4d7b1f502cafe904
Tsubasa Kondo <tsubasa.k111@gmail.com>
2020-12-21 16:24:29 +0900

    wip: initialize ipfs and orbitdb, modify NPP interface

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log('sup guys!')
-import * as newPiecePlease from './lib/newPiecePlease'
+import NPP from './lib/newPiecePlease'
 
-console.log(newPiecePlease);
+const main = async () => {
+    const nPP = await new NPP();
+    console.log(nPP.orbitdb.id)
+    console.log(nPP.pieces.id)
+};
+main();
